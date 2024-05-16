c13cce045de26ba63b5b0b9817bca9983a3317fe
zhuxiaorong <979757942@qq.com>
2024-05-16 09:44:42 +0800

    feat:集成unplugin-vue-router

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    'vue/no-reserved-component-names': 'off'
+    'vue/no-reserved-component-names': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
